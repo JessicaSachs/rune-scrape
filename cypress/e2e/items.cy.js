6dc567d03175d81ab51f196d5d57a69590b4771c
Jessica Sachs <jess@jessicasachs.io>
2022-04-04 16:18:35 -0400

    fixing ts-only failures

--- cypress/e2e/items.cy.js
+++ cypress/e2e/items.cy.js
@@ -9,33 +9,33 @@ it('works', () => {
   })
   .visit('https://www.runehq.com/item/search/A%25/name/asc/name')
   .get('.pagination').first().children()
-  // .get('[href*="/database.php?type=item"]')
-  // .then(($results) => {
-  //   const buildItem = (_, node) => {
-  //     return {
-  //       url: node.href,
-  //       id: node.href.split('&id=').pop(),
-  //       name: node.innerText
-  //     }
-  //   }
+  .get('[href*="/database.php?type=item"]')
+  .then(($results) => {
+    const buildItem = (_, node) => {
+      return {
+        url: node.href,
+        id: node.href.split('&id=').pop(),
+        name: node.innerText
+      }
+    }
 
-  //   const items = $results.map(buildItem)
+    const items = $results.map(buildItem)
   
-  //   cy.task('writeItems', items)
-  //     .wrap([items[0]]).each((i) => {
-  //       const id = i.id
-  //       cy.visit(i.url)
-  //         .get('.content-intro-image img')
-  //         .then(($imgs) => {
-  //           console.log($imgs)
-  //           cy.task('addToId', { id, data: { img: $imgs[0].src } })
-  //           .task('fetchImage', id)
-  //           .then(result => {
-  //             console.log(result);
-  //             debugger
-  //           })
-  //         })
-  //     })
-  // })
+    cy.task('writeItems', items)
+      .wrap(items).each((i) => {
+        const id = i.id
+        cy.visit(i.url)
+          .get('.content-intro-image img')
+          .then(($imgs) => {
+            console.log($imgs)
+            cy.task('addToId', { id, data: { img: $imgs[0].src } })
+            .task('fetchImage', id)
+            .then(result => {
+              console.log(result);
+              // debugger
+            })
+          })
+      })
+  })
 
 })
